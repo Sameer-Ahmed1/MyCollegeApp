54000bcfdaf98f2f37c110f2fdd54bf9f72f9258
Shubh A Chudasama <shubh.cs@proton.me>
2024-01-20 19:29:36 +0530

    Teacher screen: add search bar & list with avatar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {PaperProvider} from 'react-native-paper';
 import Home from './screens/Home';
 import Profile from './screens/Profile';
 import {RootStackParamList} from './types';
+import Teacher from './screens/Teacher';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -12,9 +13,10 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={'Home'}>
+        <Stack.Navigator initialRouteName={'Teacher'}>
           <Stack.Screen name={'Home'} component={Home} />
           <Stack.Screen name={'Profile'} component={Profile} />
+          <Stack.Screen name={'Teacher'} component={Teacher} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,18 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 export type RootStackParamList = {
   Home: undefined;
   Profile: undefined;
+  Teacher: undefined;
 };
 
 export type HomeNavigationProps = NativeStackScreenProps<
   RootStackParamList,
   'Home'
 >;
+
+export interface Teacher {
+  id: string;
+  name: string;
+  department: string;
+  profilePicUrl: string;
+  phoneNumber: string;
+}
